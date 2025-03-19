b5908078fc4747dde9b97925eb4394e85402f9f0
Anshuman <chaoanxuman999@gmail.com>
2025-03-19 18:46:53 +0530

    Initial Commit

---
+++ src/main/java/com/Test_Case_Management_System/TestCaseManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Test_Case_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCaseManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCaseManagementSystemApplication.class, args);
+	}
+
+}
